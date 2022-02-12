d42154247c47e9c8a848612a0949a05a1e56e1e0
Anti Soots <Anti@MSI.com>
2022-02-12 11:09:17 +0200

    Create Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere, mis on Teie nimi");
+            string UserName;
+            UserName = Console.ReadLine();
+            Console.WriteLine("Tere, " + UserName);
+            Console.Read();
+        }
+    }
+}
